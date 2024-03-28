7932508a7f1064295833d8cc658806a0fdfc4274
Asishkumar <gouda0682@gmail.com>
2024-03-28 14:52:14 +0530

    locators in java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -10,6 +11,29 @@ public class App {
      System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
      driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
      driver.get("http://www.wikipedia.org");
+
+     //Locators
+     //id
+     long start = System.currentTimeMillis();
+     driver.findElement(By.id("js-link-box-en"));
+     long end = System.currentTimeMillis();
+
+     System.out.println("The time needed to run this ID command: " + (end - start));
+
+     //xpath
+      start = System.currentTimeMillis();
+     driver.findElement(By.xpath("//*[@id=\"js-link-box-es\"]/strong"));
+      end = System.currentTimeMillis();
+     System.out.println("The time needed to run this xpath command: " + (end - start));
+
+
+     //cssSelector
+    start = System.currentTimeMillis();
+     driver.findElement(By.cssSelector("#js-link-box-en > strong"));
+      end = System.currentTimeMillis();
+     System.out.println("The time needed to run this cssSelector command: " + (end - start));
+
+
     }
 
 }
