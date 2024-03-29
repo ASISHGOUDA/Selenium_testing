d8fb41448d6ea4e416f2d36bcbeca4f3ba787321
Asishkumar <gouda0682@gmail.com>
2024-03-29 13:28:40 +0530

    Selenium Basics Done

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,11 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
+import java.sql.Driver;
 import java.util.concurrent.TimeUnit;
 
 public class App {
@@ -11,8 +14,76 @@ public class App {
      WebDriver driver = new ChromeDriver();
      driver.manage().window().maximize();
      System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-     driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-     driver.get("http://www.wikipedia.org");
+     driver.get("https://jqueryui.com/droppable/");
+
+
+     driver.switchTo().frame(driver.findElement(By.cssSelector("#content > iframe")));
+
+     WebElement draggable = driver.findElement(By.id("draggable"));
+     WebElement droppable = driver.findElement(By.id("droppable"));
+
+    Actions dragAndDrop = new Actions(driver);
+    dragAndDrop.dragAndDrop(draggable, droppable).build().perform();
+
+
+    Actions contextClick = new Actions(driver);
+
+    contextClick.contextClick().build().perform();
+
+    Actions actionsObject = new Actions(driver);
+    actionsObject.keyDown(Keys.F1);
+
+     //Implicit wait
+//     driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//     driver.get("https://www.udemy.com/course/core-java-programming-language-tutorial/");
+
+//     //Working with Iframes
+//     driver.get("https://www.w3schools.com/html/html_iframe.asp");
+//
+////     String title = driver.switchTo().frame(0).findElement(By.xpath("//*[@id=\"main\"]/h1")).getText();
+////     System.out.println(title);
+//        WebElement iframe = driver.findElement(By.cssSelector("#main > div:nth-child(7) > iframe"));
+//
+//        driver.switchTo().frame(iframe).findElement(By.cssSelector("#subtopnav > a:nth-child(5)")).click();
+
+//     WebElement SignupButton = driver.findElement(By.cssSelector("#udemy > div.ud-main-content-wrapper > div.ud-app-loader.ud-component--header-v6--header.ud-header.ud-app-loaded > div.ud-header.ud-text-sm.desktop-header-module--header--a5wri.desktop-header-module--flex-middle--k0Teb > div:nth-child(9) > a > span"));
+//
+//     //clicking with JavascriptExecutor
+//     ((JavascriptExecutor) driver).executeScript("arguments[0].click();, SignupButton");
+//
+//     //Setting the timeout
+//     ((JavascriptExecutor) driver).executeAsyncScript("window.setTimeout[arguments.length-1], 1000");
+//
+//     //changing the webpage
+//     ((JavascriptExecutor) driver).executeScript("window.location = `https://wikipedia.com`");
+
+
+
+     //Explicit wait
+//     WebDriverWait wait = new WebDriverWait(driver, 10);
+//     wait.until(ExpectedConditions.presenceOfElementLocated(By.id("js-link-box-en")));
+//
+//    //Fluent wait
+//     FluentWait<WebDriver> wait = new FluentWait<>(driver)
+//             .withTimeout(Duration.ofSeconds(10))
+//             .pollingEvery(Duration.ofSeconds(1))
+//             .ignoring(NoSuchElementException.class);
+//
+//     try {
+//         // Wait for the element with the specified ID to be present
+//         WebElement element = wait.until(driver -> driver.findElement(By.id("your_element_id")));
+//
+//         // If the element is found, perform actions on it
+//         // For example, you can click on it
+//         element.click();
+//     } catch (TimeoutException e) {
+//         // If the element is not found within 10 seconds, handle the error
+//         System.out.println("Element not found within 10 seconds!");
+//         e.printStackTrace();
+//     } finally {
+//         // Quit the driver
+//         driver.quit();
+//     }
 
      //Locators
      //id
@@ -78,8 +149,10 @@ public class App {
 //     List<WebElement> listOfWebElements = driver.findElements(By.xpath("/html/body/table/tbody[1]/tr"));
 //
 //     for(WebElement element : listOfWebElements){
-//         System.out.println(element.getText());
+//         System.out.println(element.());
 //     }
+
+
 
 
 
